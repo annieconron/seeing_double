a713af43ff8fbd2c5dade7d3d3585cf53567d044
annie conron <s3900816@student.rmit.edu.au>
2023-06-02 10:50:49 +1000

    new push

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,38 @@
-document.body.style.margin   = 0
-document.body.style.overflow = `hidden`
+let capture;
+let r, g, b;
 
-function setup () {
-   createCanvas (innerWidth, innerHeight)
+function setup() {
+  createCanvas(800, 600);
+  capture = createCapture(VIDEO);
+  capture.size(width, height);
+  capture.hide();
+  noStroke();
+  background(255);
+  rectMode(CENTER);
+  
+  // Initialize RGB values
+  r = 0;
+  g = 0;
+  b = 0;
 }
 
-function  draw () {
-   background (`turquoise`)
+function draw() {
+  for (let i = 0; i < 30; i++) {
+    const x = random(width);
+    const y = random(height);
+    const c = capture.get(x, y);
+
+    const videoR = red(c);
+    const videoG = green(c);
+    const videoB = blue(c);
+
+    const col = color(videoR, videoG, videoB, 30);
+    fill(col);
+    circle(x, y, random(20, 60));
+  }
 }
+
+function mousePressed() {
+  clear();
+}
+
